224b67d8c3e278fdc5858c06e18f7093c14cdb69
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-25 08:23:43 +0600

    add day-2 variables

--- starter_app/lib/main.dart
+++ starter_app/lib/main.dart
@@ -5,7 +5,15 @@ void main() {
 }
 
 class StarterApp extends StatelessWidget {
-  const StarterApp({Key? key}) : super(key: key);
+  //const StarterApp({Key? key}) : super(key: key);
+
+  //Variables in dart
+  // int age = 28; //integer type number
+  // double pi = 3.14; //fractional number
+  // num n = 24; //will take any number and auto detect its type
+  // bool isTrue = true; //only true or false
+  String name = 'flutter'; //sequence of charracter
+  var anyType = 2; //takes any valid type dada and auto assign its data type
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +21,7 @@ class StarterApp extends StatelessWidget {
       home: Material(
         child: Center(
           child: Container(
-            child: Text("hello flutter at day 1"),
+            child: Text("hello $name at day $anyType"),
           ),
         ),
       ),
