e41dd065d15782c77944075934ceac4d76a64487
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-27 15:05:31 +0600

    Add light theme

--- starter_app/lib/main.dart
+++ starter_app/lib/main.dart
@@ -18,11 +18,16 @@ class StarterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
-      themeMode: ThemeMode.dark, //Dark theme
-      darkTheme: ThemeData(
-        //brightness: Brightness.dark, //describes the color of a theme
-        primarySwatch: Colors
-            .red, //if the brightness is set to dark , primary swatch won't work
+      // themeMode: ThemeMode.dark, //Dark theme
+      // darkTheme: ThemeData(
+      //   //brightness: Brightness.dark, //describes the color of a theme
+      //   primarySwatch: Colors
+      //       .red, //if the brightness is set to dark , primary swatch won't work
+      // ),
+
+      themeMode: ThemeMode.light, //light theme
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
       ),
     );
   }
