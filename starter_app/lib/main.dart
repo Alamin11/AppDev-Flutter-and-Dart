e9ba9832f3727d93c71f4d01c1b1fac828d879d5
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-27 14:55:55 +0600

    Add dark them

--- starter_app/lib/main.dart
+++ starter_app/lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:starter_app/pages/home_page.dart';
 
 void main() {
-  runApp(StarterApp());
+  runApp(const StarterApp());
 }
 
 class StarterApp extends StatelessWidget {
-  //const StarterApp({Key? key}) : super(key: key);
+  const StarterApp({Key? key}) : super(key: key);
 
   //Variables in dart
   // int age = 28; //integer type number
@@ -18,6 +18,12 @@ class StarterApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      themeMode: ThemeMode.dark, //Dark theme
+      darkTheme: ThemeData(
+        //brightness: Brightness.dark, //describes the color of a theme
+        primarySwatch: Colors
+            .red, //if the brightness is set to dark , primary swatch won't work
+      ),
     );
   }
 }
